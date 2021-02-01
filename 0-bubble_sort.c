24a11f0e25214a7b4a2c22fd5d3fd267a351d6a1
Carlos Araque <cararqueu@gmail.com>
2021-02-01 16:02:04 +0000

    fixs

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -10,27 +10,22 @@
 
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, j, u;
-	int tmp; /* Temporal variable for swap */
+	size_t i, j, tmp;
 
-	if (array == NULL || size < 2)
+	if (size < 2 || array == NULL)
 		return;
 
-	j = size;
-	while (j > 0)
+	for(i = 0; i < size - 1; i++)
 	{
-		u = 0;
-		for (i = 0; i < j - 1; i++)
+		for (j = 0; j < size - 1; j++)
 		{
-			if (array[i] > array[i + 1])
+			if (array[j] > array[j + 1])
 			{
-				tmp = array[i];
-				array[i] = array[i + 1];
-				array[i + 1] = tmp;
-				u = i + 1;
+				tmp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = tmp;
 				print_array(array, size);
 			}
 		}
-		j = u;
 	}
 }
