a936357f8b7513f10641e4c5d7405791af2de430
Carlos Araque <cararqueu@gmail.com>
2021-02-02 21:46:27 +0000

    task 3

--- sort.h
+++ sort.h
@@ -27,4 +27,6 @@ listint_t *create_listint(const int *array, size_t size);
 void swap_nodes(listint_t **head, listint_t *node, listint_t *next_node);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+void shell_sort(int *array, size_t size);
 #endif
